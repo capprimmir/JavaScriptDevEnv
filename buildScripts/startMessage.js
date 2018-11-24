faf0a44fbb38eec961a603586b08393bb7ab2f2e
Mariana-Perez <Mariana.Perez@ibm.com>
2018-11-24 15:39:52 -0500

    added babelrc file and modify startMessage file

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk'); //library to display content in color
+import chalk from 'chalk'; //library to display content in color
 
 console.log(chalk.green('Starting app in dev mode...'));
