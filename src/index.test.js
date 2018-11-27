611d173d8a0d47ec6892c9e68c21dc2f82730bd0
Mariana-Perez <Mariana.Perez@ibm.com>
2018-11-27 09:38:12 -0500

    module 9 work in progress

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,28 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () =>{
   it('should pass', () => {
     expect(true).to.equal(true);
   });
 });
+
+//test using jsdom
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    //content of index.html hold in memory
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    // create a virtula DOM in mmeory
+    jsdom.env(index, function(err, window){
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello Mariana");
+      done();
+      window.close();
+    })
+  })
+})
+
+
+// note: need to use done() when having asynchronous call function, in this case the
+// function in jsdom
