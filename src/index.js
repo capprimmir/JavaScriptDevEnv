000b47ff437285e8594aff1e31e8a41c02ffd14a
Mariana-Perez <Mariana.Perez@ibm.com>
2018-12-10 17:08:06 -0500

    starting production build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getUsers } from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 //Populate table of users via API call
 getUsers().then(result => {
@@ -6,12 +6,25 @@ getUsers().then(result => {
 
   result.forEach(user => {
     usersBody += ` <tr>
-      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete/a></td>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
       <td>${user.email}</td>
       </tr>`
   });
-  global.document.getElementById('users').innerHTML= usersBody;
-})
+  window.document.getElementById('users').innerHTML= usersBody;
+
+  const deleteLinks = window.document.getElementsByClassName('deleteUser');
+
+  // use Array.from to create an array from a DOM collection
+Array.from(deleteLinks, link => {
+  link.onclick = function(event) {
+    const element = event.target;
+    event.preventDefault();
+    deleteUser(element.attributes["data-id"].value);
+    const row = element.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+  };
+});
+});
